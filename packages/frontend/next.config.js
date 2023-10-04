c06e81473b398839eb9cb91ae0dc351f63de3c6e
huongnguyen1602 <nguyenhuuhuong1602@gmail.com>
2023-10-04 09:32:57 +0700

    update frontend

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,13 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-env node */
-// @ts-check
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
 
-/**
- * @type {import('next').NextConfig}
- **/
-let nextConfig = {}
-
-const withTwin = require('./withTwin.js')
-nextConfig = withTwin(nextConfig)
-
-module.exports = nextConfig
+module.exports = nextConfig;
--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,22 +1,52 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-env node */
-// @ts-check
-
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    colors: {
+      current: "currentColor",
+      transparent: "transparent",
+      white: "#FFFFFF",
+      black: "#090E34",
+      dark: "#1D2144",
+      primary: "#4A6CF7",
+      yellow: "#FBB040",
+      "body-color": "#959CB1",
+    },
+    screens: {
+      xs: "450px",
+      // => @media (min-width: 450px) { ... }
+
+      sm: "575px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1400px",
+      // => @media (min-width: 1400px) { ... }
+    },
     extend: {
-      colors: {
-        gray: colors.zinc,
-      },
-      fontFamily: {
-        mono: ['var(--font-inconsolata)', ...defaultTheme.fontFamily.mono],
+      boxShadow: {
+        signUp: "0px 5px 10px rgba(4, 10, 34, 0.2)",
+        one: "0px 2px 3px rgba(7, 7, 77, 0.05)",
+        sticky: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)",
       },
     },
   },
   plugins: [],
-}
+};
