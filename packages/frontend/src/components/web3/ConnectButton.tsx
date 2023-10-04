3fa7610478a26951c7b63e56975f5a614e650a9f
lamha79 <lamha.24h@gmail.com>
2023-10-04 20:48:29 +0700

    fix lib

--- packages/frontend/src/components/web3/ConnectButton.tsx
+++ packages/frontend/src/components/web3/ConnectButton.tsx
@@ -12,7 +12,7 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react'
-import { env } from '@config/environment'
+import { env } from '../config/environment'
 import { InjectedAccount } from '@polkadot/extension-inject/types'
 import { encodeAddress } from '@polkadot/util-crypto'
 import {
@@ -24,8 +24,8 @@ import {
   useBalance,
   useInkathon,
 } from '@scio-labs/use-inkathon'
-import { truncateHash } from '@utils/truncateHash'
-import { useIsSSR } from '@utils/useIsSSR'
+import { truncateHash } from '../../utils/truncateHash'
+import { useIsSSR } from '../../utils/useIsSSR'
 import Image from 'next/image'
 import aznsIconSvg from 'public/icons/azns-icon.svg'
 import { FC, useMemo, useState } from 'react'
