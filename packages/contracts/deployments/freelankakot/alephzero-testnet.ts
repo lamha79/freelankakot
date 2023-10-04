fd1093a5e430b4f2effeb746030183143a4f7d28
huongnguyen1602 <nguyenhuuhuong1602@gmail.com>
2023-10-04 22:58:50 +0700

    update

--- packages/contracts/deployments/freelankakot/alephzero-testnet.ts
+++ packages/contracts/deployments/freelankakot/alephzero-testnet.ts
@@ -1,2 +1,2 @@
-export const address = '5CrtpVyLB8iaRPJyB39DvfGgHP1BLGpEfmb9XF1qzN5CFQbX'
-export const blockNumber = 41737105
+export const address = '5DqupDitHjvZaatu3Rb6PsqL38vSnivt7CfFp4664JZL2HZw'
+export const blockNumber = 43182803
