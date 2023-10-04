65c6b29e054ec3267adb3888a74642d7e16fc24f
kienngynh <kien.ngynh@gmail.com>
2023-10-04 23:59:39 +0700

    Update get_all_open_jobs function smart contract

--- packages/frontend/src/pages/createjob.tsx
+++ packages/frontend/src/pages/createjob.tsx
@@ -19,57 +19,59 @@ export default function CreateJobPage() {
         <title>Create Job Page</title>
       </Head>
 
-      <section className="text-center py-10">
-        <h1 className="text-3xl text-gray-800 mb-6">Create Job</h1>
-        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="jobTitle">
-              Job Title
-            </label>
-            <input
-              type="text"
-              id="jobTitle"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter the job title"
-              value={jobTitle}
-              onChange={(e) => setJobTitle(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
-              Description
-            </label>
-            <textarea
-              id="description"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter the job description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="budget">
-              Budget
-            </label>
-            <input
-              type="text"
-              id="budget"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter the budget"
-              value={budget}
-              onChange={(e) => setBudget(e.target.value)}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Create Job
-          </button>
-        </form>
+      <section className="py-10">
+        <div className="bg-white rounded-lg shadow p-6 mx-auto max-w-3xl">
+          <h1 className="text-3xl text-gray-800 font-bold mb-6">Create a Job</h1>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label className="text-gray-700 font-semibold mb-2" htmlFor="jobTitle">
+                Job Title
+              </label>
+              <input
+                type="text"
+                id="jobTitle"
+                className="border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-500"
+                placeholder="Enter the job title"
+                value={jobTitle}
+                onChange={(e) => setJobTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="text-gray-700 font-semibold mb-2" htmlFor="description">
+                Description
+              </label>
+              <textarea
+                id="description"
+                className="border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-500"
+                placeholder="Enter the job description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="text-gray-700 font-semibold mb-2" htmlFor="budget">
+                Budget
+              </label>
+              <input
+                type="text"
+                id="budget"
+                className="border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-500"
+                placeholder="Enter the budget"
+                value={budget}
+                onChange={(e) => setBudget(e.target.value)}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded focus:outline-none focus:ring focus:border-blue-300"
+            >
+              Post Job
+            </button>
+          </form>
+        </div>
       </section>
     </Layout>
   );
--- packages/frontend/src/pages/searchjobs.tsx
+++ packages/frontend/src/pages/searchjobs.tsx
@@ -9,11 +9,13 @@ export default function SearchJobPage() {
         <title>Search Job Page</title>
       </Head>
 
-      <section className="text-center py-10">
-        <h1 className="text-2xl">
-          <FaCode className="inline-block mr-2" /> Search Job Page
-        </h1>
-        {/* Your search job form and results can be added here */}
+      <section className="py-10">
+        <div className="bg-white rounded-lg shadow p-6 mx-auto max-w-3xl">
+          <h1 className="text-3xl text-gray-800 font-bold mb-6">
+            <FaCode className="inline-block align-middle text-blue-500 mr-2" /> Find Jobs
+          </h1>
+          {/* Your search job form and results can be added here */}
+        </div>
       </section>
     </Layout>
   );
