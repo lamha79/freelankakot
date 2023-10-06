fcbd890b16f81063653169d886bee81646aae446
lamha79 <lamha.24h@gmail.com>
2023-10-06 20:29:48 +0700

    clean

--- packages/frontend/src/contracts/deployments/freelankakot/alephzero-testnet.ts
+++
@@ -1,2 +0,0 @@
-export const address = '5FLbpBNPww3wyfhKYFKecSw3SdQvEkaQbSWCVAaGYuqRWrRX'
-export const blockNumber = 43188419
--- packages/frontend/src/front-provider/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './user-provider';
-export * from './landing-provider';
-export * from './company-provider';
-export * from './jobs-provider';
-export * from './chat-provider';
-export * from './api';
